141fdddf9c8c23fde487e216156c8bc4c6161679
grac2801 <oscar.gracias@icloud.com>
2023-10-13 14:20:32 -0700

    started simple ifs

--- unit_03_decisionMaking/U3_L2_simpleIfs.py
+++ unit_03_decisionMaking/U3_L2_simpleIfs.py
@@ -14,4 +14,27 @@
 
 
 if __name__ == '__main__':
-    pass
+    print('\n\n********** simple if **********')
+    language = input('What is your favorite programming language? ')
+    if(language == 'Python'):
+        print('I knew Python was your favorite.')
+    print('Done')
+    
+    
+    print('\n\n********** equality operators **********')
+    notes = '''
+    == equal to
+    <= less than or equal to
+    >= greater than or equal to
+    !== not equal to
+    '''
+    print(notes)
+    
+    print('\n\n********** student exercise **********')
+    #===========================================================================
+    # prompt user to enter an integer
+    # if integer is > 100, output so
+    #===========================================================================
+    number = int(input('Enter an integer: '))
+    if number >= 100:
+        print('Integer > 100')
