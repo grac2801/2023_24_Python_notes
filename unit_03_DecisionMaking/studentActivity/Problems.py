46bb806859e9c201f3b87b6d7b9b8ecd89c09db4
ogracias <oscar.gracias@icloud.com>
2023-10-18 08:30:04 -0700

    Uploaded activity file with comments to complete

---
+++ unit_03_DecisionMaking/studentActivity/Problems.py
@@ -0,0 +1,128 @@
+# Oct 14, 2023
+# Author: mrgracias
+
+
+#imports
+
+
+
+#functions
+
+
+
+if __name__ == '__main__':
+    print('\n\n********** Activity 1: Password **********')
+    #=========================================================================
+    # Student activity 1:
+    # prompt the user for a password. 
+    # check if correct.
+    # give user 3 chances only
+    # give user a message of welcoming or
+    # alert police
+    #=========================================================================
+    
+            
+            
+            
+            
+   
+    print('\n\n********** Activity 2: [Dog years] **********')
+    #============================================================================
+    #    Student activity 2:
+    # It is commonly said that one human year is equivalent to 7 dog years,
+    # However, this simple conversion fails to recognize that dogs reach 
+    # adulthood in approximately 2 years. 
+    # As a result, some people believe that it is better to count each of the 
+    # first 2 human years ad 10.5 dog years, and then count each additional 
+    # human year a 4 dog years.
+    # Write a program that implements the conversion from human years to 
+    # dog years described. Ensure your program works correctly.
+    # Should a negative value be input, the program should advise user of the
+    # issue.
+    #============================================================================
+    
+    
+    
+    
+    
+
+    
+    
+    print('\n\n********** Activity 3: Month length **********')
+    #===========================================================================
+    #     Student activity 3:
+    # The length of a month varies from 28-31 days. Create a program which
+    # reads the name of a month from user as a string, then your program 
+    # should display the number of days in that month.
+    # Display "28 or 29 days" for February.
+    # http://www.tovresearch.com/definitions/daymonthchart.html
+    #===========================================================================
+    
+    
+    
+    
+    
+        
+
+    print('\n\n********** Activity 4: Holidays **********')
+    #===================================================================
+    # Student activity 4:
+    # Write program that reads a month and day from the user. If the month
+    # and day match one of the holidays listed, then your program should display the 
+    # holiday's name. 
+    #
+    # New Year’s Day January 1
+    # Martin Luther King’s Birthday January 1st
+    # President’s Day February 20 
+    # Memorial Day May 19
+    # Juneteenth National Independence Day June 19
+    # Independence Day July 4
+    # Labor Day September 4
+    #  Day October 9
+    # Veterans’ Day November 11
+    # Thanksgiving Day November 23
+    # Christmas Day December 25
+    #===================================================================
+    
+    
+    
+    
+    
+
+    print('\n\n********** Activity 5: Kid quiz **********')
+    #===========================================================================
+    #student activity 5
+    #
+    # Make a program which asks students in elementary school
+    # 5 multiple choice questions, checks how many the student 
+    # got right.
+    # 1-Provides feedback of updated score per question answered
+    # 2-Provides a percent grade at the end of the algorithm
+    #===========================================================================
+    
+    
+    
+    
+    
+    
+    print('\n\n********** Activity 6: Zodiac signs **********')
+    #============================================================================
+    # Student activity 6:
+    # Write a program which asks the user to enter his or her month and day of 
+    # birth. Then, your program should report the user's zodiac sign as part of an
+    # appropriate output message.
+    #
+    # Aries: March 21 - April 19
+    # Taurus: April 20 - May 20
+    # Gemini: May 21 - June 20
+    # Cancer️: June 21 - July 22
+    # Leo️: July 23 - August 22
+    # Virgo: August 23 - September 22
+    # Libra️: September 23 - October 22
+    # Scorpio: October 23 - November 21
+    # Sagittarius️: November 22 - December 21
+    # Capricorn: December 22 - January 19
+    # Aquarius: January 20 - February 18
+    # Pisces️: February 19 - March 20
+    #============================================================================
+    
