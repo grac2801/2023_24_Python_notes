48f508f15c9d249d9a8bd2ca611b6ca4efb532ca
ogracias <oscar.gracias@icloud.com>
2023-10-25 08:28:17 -0700

    04_07_practice file for students

---
+++ unit_04_loops/04_07_practice.py
@@ -0,0 +1,54 @@
+# imports
+
+
+
+# functions
+
+
+if __name__ == '__main__':
+    print('\n\n********** Case 1 **********')
+    '''
+    #1 which type of loop?
+    Print python 10 times. One with a count variable
+    or user input
+    '''
+    
+    
+    
+    
+    print('\n\n********** Case 2 **********')
+    '''
+    #2 prompt the user for 5 numbers, state when the number
+    is divisible by 3. Show the total sum of these numbers at the end.
+    '''
+    
+    
+    
+    
+    
+    
+    print('\n\n********** Case 3 **********')
+    '''
+    #3 Have the user input a number > 100, and
+    repeat until they do
+    '''
+    
+    
+    
+    
+    
+    
+    print('\n\n********** Case 4 **********')
+    '''
+    #4 user to enter in numbers and then average
+    #them
+    '''
+    
+    
+    
+    
+    print('\n\n********** Case 5 **********')
+    '''
+    Randomize 3 unicode symbols, 5 times. Show the name of the symbol
+    ''' 
+    
