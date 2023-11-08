50b432dabe3d24acbf0464b1bcf36049d0ae06e5
grac2801 <oscar.gracias@icloud.com>
2023-11-08 10:41:10 -0800

    class on library

---
+++ unit_07_functions/library/operationsClient.py
@@ -0,0 +1,30 @@
+# Nov 8, 2023
+# Author: ogracias
+
+
+# imports
+import unit_07_functions.library.basicOperationsLibrary as lib
+
+
+
+
+# functions
+
+
+
+
+if __name__ == '__main__':
+    totalSum = lib.add(5, 6)
+    print(totalSum)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
